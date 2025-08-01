d6c8cb5e75171ddfb80009cd38fd39e1c038adce
nicolas <nicolasatoyebi@gmail.com>
2025-08-01 21:42:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// In-memory array to store tasks
+let tasks = [];
+
+// DOM elements
+const taskInput = document.getElementById("task-input");
+const addButton = document.getElementById("add-button");
+const taskList = document.getElementById("task-list");
+
+// Event listener for adding a task
+addButton.addEventListener("click", () => {
+	// TODO: call addTask()
+});
+
+// TODO: function addTask(text) {}
+
+// TODO: function renderTasks() {}
+
+// TODO: function createTaskElement(task) {}
